98cba9c32111872fae7511242e72451187ed74cf
vnrgh <dandrabskiy@gmail.com>
2025-08-14 15:34:04 +0300

    added customer microservice

---
+++ customer/src/main/java/com/vnrgh/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,5 @@
+package com.vnrgh.customer;
+
+public record CustomerRegistrationRequest(String firstName,
+                                          String lastName,
+                                          String email) {}
---
+++ customer/src/main/java/com/vnrgh/customer/CustomerService.java
@@ -0,0 +1,15 @@
+package com.vnrgh.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository repository) {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        repository.save(customer);
+    }
+}
