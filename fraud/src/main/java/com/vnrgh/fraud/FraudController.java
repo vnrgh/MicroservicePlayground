16c35be1b201d30aedfadd68e44f37140f5f6413
vnrgh <dandrabskiy@gmail.com>
2025-08-16 13:59:19 +0300

    added Eureka service discovery

--- fraud/src/main/java/com/vnrgh/fraud/FraudController.java
+++ fraud/src/main/java/com/vnrgh/fraud/FraudController.java
@@ -1,9 +1,11 @@
 package com.vnrgh.fraud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService service;
@@ -15,6 +17,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     private FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = service.isFraudulentCustomer(customerId);
+        log.info("FRAUD CHECK REQUEST FOR CUSTOMER {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
